3020093efb2d181e254c42d19a44238418d7ad56
shri <shri@DESKTOP-D8K2FMD>
2023-10-04 01:34:01 +0530

    First Commit

--- Radhika/src/com/tnsif/dayone/FirstProgram.java
+++
@@ -1,8 +0,0 @@
-//Demonstration of simple java program
-package com.tnsif.dayone;
-
-public class FirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
